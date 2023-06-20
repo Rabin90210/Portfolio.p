3fd66f12a8f9a498b5d7557cb9d0e65fae71f616
Rabin90210 <rabinbhandari1213@hotmail.com>
2023-06-21 01:22:04 +1000

    contact page

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
-template_fmtsavj
-service_swkbe7i
-plB9qYpHPzy49y7SD
 
+
+
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector(".modal__overlay--loading");
+    const success = document.querySelector(".modal__overlay--success");
+    loading.classList += " modal__overlay--visible";
+    emailjs
+      .sendForm(
+        "service_swkbe7i",
+        "template_fmtsavj",
+        event.target,
+        "z2AltRxOBWCRHCPMd"
+      )
+      .then(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+      })
+      .catch(() => {
+        loading.classList.remove("modal__overlay--visible");
+        alert(
+          "The email service is temporarily unavailable. Please contact me directly on rabinbhandari37@gmail.com"
+        );
+      });
+
+    }
+
+    let isModalOpen = false;
+    function toggleModal() {
+        if (isModalOpen) {
+          isModalOpen = false;
+          return document.body.classList.remove("modal--open");
+        }
+        isModalOpen = true;
+        document.body.classList += " modal--open";
+      }  
