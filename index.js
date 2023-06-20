16956f2cde64bc064b3424d48c1b9b0361d43b14
Rabin90210 <rabinbhandari1213@hotmail.com>
2023-06-21 02:12:40 +1000

    Dark theme

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
 
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    // Added rotate after 
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px) rotate(${x * boolInt * 10}deg)`
+  }
+}
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme"
+  }
+  else {
+    document.body.classList.remove("dark-theme")
+  }
+}
 
 
 function contact(event) {
@@ -26,7 +51,7 @@ function contact(event) {
 
     }
 
-    let isModalOpen = false;
+
     function toggleModal() {
         if (isModalOpen) {
           isModalOpen = false;
